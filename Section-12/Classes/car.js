d9cb4ad44e42abf022beb89d169c1bb50fb0817b
mmahoney6713 <mrm257@cornell.edu>
2020-12-16 23:13:37 -0500

    completed exercise

--- Section-12/Classes/car.js
+++ Section-12/Classes/car.js
@@ -1,3 +1,51 @@
-window.onload = () => {
 
+
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return 'Beep.';
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return 'VROOM!!!';
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+    add(newVehicle) {
+        if (newVehicle instanceof Vehicle) {
+            if (this.vehicles.length === this.capacity) {
+                return "Sorry, we're full.";
+            } else {
+                this.vehicles.push(vehicle);
+                return "Vehicle added!";
+            }
+        } else {
+            return "Only vehicles are allowed in here!";
+        }
+    }
 }
