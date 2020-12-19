027c51b38bb00a36ad0ac5da2cb5ad5dc161185c
mmahoney6713 <mrm257@cornell.edu>
2020-12-19 14:00:57 -0500

    updated movie app with sorting

--- Section-13/Movie-app/app.js
+++ Section-13/Movie-app/app.js
@@ -2,24 +2,59 @@
 // Create an array to store movie objects into
 const movies = [];
 
+const addMovieToDom = (title, rating) => {
+    const newMovieDiv = $(`<div>${title} | ${rating} </div>`).appendTo($('section'));
+    addDeleteBtn(newMovieDiv);
+    return newMovieDiv;
+}
+
+const addDeleteBtn = (newMovieDiv) => {
+    $('<button>Remove</button>').addClass('deleteBtn').appendTo(newMovieDiv);
+}
+
+const sortMoviesArray = (sortType, direction) => {
+    movies.sort((a, b) => {
+        if (sortType === 'title') {
+            if (a[0].toLowerCase() > b[0].toLowerCase()) {
+                return parseInt(`${direction}1`);
+            } else {
+                return parseInt(`${direction}1`) * -1;
+            }
+        } else {
+            if (parseFloat(a[1]) > parseFloat(b[1])) {
+                return parseInt(`${direction}1`);
+            } else {
+                return parseInt(`${direction}1`) * -1;
+            }
+        }
+    })
+
+    sortMoviesHTML();
+
+}
+
+function sortMoviesHTML() {
+    $('section div').remove();
+    for (let movie of movies) {
+        addMovieToDom(movie[0], movie[1])
+    }
+}
+
 $(function () {
+
+    /////// EVENT LISTENERS ///////
 
     $('form').on('submit', function (event) {
         event.preventDefault();
         const title = $('#title').get()[0].value;
         const rating = $('#rating').get()[0].value;
         if (title && rating) {
-            const newMovieDiv = $(`<div>${title} | ${rating} </div>`).appendTo($('section'));
-            addDeleteBtn(newMovieDiv);
-            movies.push(newMovieDiv);
+            const newMovieDiv = addMovieToDom(title, rating);
+            movies.push([title, rating, newMovieDiv]);
         }
         $('#title').val('');
         $('#rating').val('');
     })
-
-    const addDeleteBtn = (newMovieDiv) => {
-        $('<button>Remove</button>').addClass('deleteBtn').appendTo(newMovieDiv);
-    }
 
     $('section').on('click', '.deleteBtn', function () {
         $(this).parent().remove();
@@ -28,20 +63,7 @@ $(function () {
     $('section').on('click', '.sortBtn', function () {
         const sortType = $(this).attr('class').split(' ')[1];
         const direction = $(this).attr('class').split(' ')[2];
-        sortMoviesByType(sortType, direction);
+        sortMoviesArray(sortType, direction);
     })
-
-    const sortMoviesByType = (sortType, direction) => {
-        if (sortType === 'titles') {
-            movies.sort((a, b) => a - b);
-        } else {
-            movies.sort((a, b) => b - a);
-        }
-    }
-
-    const sortMoviesByDirection = (direction) => {
-
-    }
-
 
 })
