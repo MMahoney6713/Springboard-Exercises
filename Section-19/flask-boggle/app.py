608b480962119d978b32d7472fa8f3e5950e40ff
mmahoney6713 <mrm257@cornell.edu>
2021-01-04 21:10:11 -0500

    board shows and now starting to get the words to show up

--- Section-19/flask-boggle/app.py
+++ Section-19/flask-boggle/app.py
@@ -1,3 +1,30 @@
 from boggle import Boggle
+from flask import Flask, render_template, session, request, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "chickens"
 
 boggle_game = Boggle()
+board = boggle_game.make_board()
+
+
+@app.route('/')
+def show_board():
+    session['board'] = board
+    session['words'] = []
+    return redirect('/play')
+
+
+@app.route('/play')
+def play_boggle():
+    return render_template('base.html')
+
+
+@app.route('/guess', methods=['POST'])
+def check_guess():
+    guess_word = request.form['guess']
+    if boggle_game.check_valid_word(board, guess_word) == 'ok':
+        words = session['words']
+        words.append(guess_word)
+        session['words'] = words
+    return redirect('/play')
